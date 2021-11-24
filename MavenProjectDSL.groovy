8b8957845ccebe700681284e34dba94403793516
terasuri <79835974+terasuri@users.noreply.github.com>
2021-11-24 18:18:07 +0530

    4

--- MavenProjectDSL.groovy
+++ MavenProjectDSL.groovy
@@ -7,7 +7,7 @@ job('First-Maven-Project-Via-DSL_2') {
         scm('* * * * *')
     }
     steps {
-        maven('Suri-maven'), maven('clean package', 'maven-samples/single-module/pom.xml')
+        maven('clean package', 'maven-samples/single-module/pom.xml')
     }
     publishers {
         //archive the war file generated
